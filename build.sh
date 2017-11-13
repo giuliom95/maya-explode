34170e17a3f4724356f60d9bb8ee92fb3a6aedaa
Giulio Martella <giulio@localworkstation.localdomain>
2017-11-13 14:50:30 +0100

    Modified build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 SRC="node"
-STD="-std=c++11"
+OUT="explode"
+STD="c++11"
 
-g++ $STD -c -I/usr/autodesk/maya2017/include -fPIC src/$SRC.cpp
-g++ $STD -shared -o $SRC.so $SRC.o -L/usr/autodesk/maya2017/lib -lOpenMaya
+g++ -std=$STD -c -I/usr/autodesk/maya2017/include -fPIC src/$SRC.cpp
+g++ -std=$STD -shared -o $OUT.so *.o -L/usr/autodesk/maya2017/lib -lOpenMaya
 rm *.o
